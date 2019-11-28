d1be32d24aacfb245c83b4874b3178ac3dbf1550
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-11-28 15:24:27 +0300

    Add keycodes

---
+++ includes/mlx_keys.h
@@ -0,0 +1,32 @@
+#ifndef MLX_KEYS_H
+#define MLX_KEYS_H
+
+# define MLX_K_A 0
+# define MLX_K_S 1
+# define MLX_K_D 2
+# define MLX_K_X 7
+# define MLX_K_C 8
+# define MLX_K_Q 12
+# define MLX_K_W 13
+# define MLX_K_E 14
+# define MLX_K_R 15
+# define MLX_K_T 17
+# define MLX_K_P 35
+# define MLX_K_ESC 53
+# define MLX_K_1 18
+# define MLX_K_2 19
+# define MLX_K_3 20
+# define MLX_K_4 21
+# define MLX_K_5 23
+# define MLX_K_6 22
+# define MLX_K_7 26
+# define MLX_K_8 28
+# define MLX_K_9 25
+
+# define MLX_M_LEFT 1
+# define MLX_M_RIGHT 2
+# define MLX_M_MIDDLE 3
+# define MLX_M_SCROLL_UP 4
+# define MLX_M_SCROLL_DOWN 5
+
+#endif
