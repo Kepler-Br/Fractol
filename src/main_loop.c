7eb63beca64524cbbbd79204c6762ab5f7830a35
Barrett Kepler <barrett.kepler@gmail.com>
2019-11-28 15:25:06 +0300

    main stuff

--- src/main_loop.c
+++ src/main_loop.c
@@ -1,13 +1,16 @@
-#include "main_loop.h"
 #include <unistd.h>
-#include "mlx.h"
+#include <mlx.h>
+#include "main_loop.h"
+#include "state.h"
 
 static int loop(struct s_main_loop *this)
 {
+	this->render(this);
 	return (0);
 }
 static int render(struct s_main_loop *this)
 {
+	this->state->render(this->state);
 	return (0);
 }
 static void run(struct s_main_loop *this)
@@ -45,25 +48,27 @@ t_main_loop *t_main_loop_destroy(struct s_main_loop *this)
 t_main_loop *t_main_loop_create(char *title, cl_uint2 window_geometry, struct s_state *state)
 {
 	t_main_loop		*object;
-	t_mlx_instance	instance;
+	t_mlx_instance	mlx_instance;
 
 	if(!(object = (t_main_loop*)malloc(sizeof(t_main_loop))))
 	{
 		write(1, "t_main_loop_create: could not allocate memory.", 46);
 		exit(1);
 	}
-	if(!(instance.mlx = mlx_init()))
+	if(!(mlx_instance.mlx = mlx_init()))
 	{
 		write(1, "t_main_loop_create: cannot init mlx.", 36);
 		exit(1);
 	}
-	if(!(instance.window = mlx_new_window(instance.mlx, window_geometry.x, window_geometry.y, title)))
+	if(!(mlx_instance.window = mlx_new_window(mlx_instance.mlx, window_geometry.x, window_geometry.y, title)))
 	{
 		write(1, "t_main_loop_create: cannot init mlx window.", 43);
 		exit(1);
 	}
-	instance.window_geometry = window_geometry;
-	object->mlx_instance = instance;
+	object->state = state;
+	state->mlx_instance = mlx_instance;
+	mlx_instance.window_geometry = window_geometry;
+	object->mlx_instance = mlx_instance;
 
 	object->loop = &loop;
 	object->render = &render;
