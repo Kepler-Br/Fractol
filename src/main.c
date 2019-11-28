cc71cb5051a7cc26e1d52547eccd4919736174ae
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-11-28 15:25:06 +0300

    main stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "main_loop.h"
+#include "raymarch_state.h"
 
 int main()
 {
-	t_main_loop *main_loop = t_main_loop_create("Test test", (cl_uint2){1000, 1000}, NULL);
+	t_state *raymarch_state = t_raymarch_state_create();
+	t_main_loop *main_loop = t_main_loop_create("Test test", (cl_uint2){1000, 1000}, raymarch_state);
 	main_loop->run(main_loop);
 	return 0;
 }
